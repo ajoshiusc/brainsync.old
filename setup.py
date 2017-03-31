3c7e0fdeee4db3cf42d50978d82c018e82b8ba76
Anand A Joshi <ajoshi@usc.edu>
2017-03-31 01:29:51 -0700

    Initial version of BrainSync.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='brainsync',
+      version='0.1',
+      description='Tools for comparing fMRI data',
+      url='http://github.com/ajoshiusc/brainsync',
+      author='Anand A Joshi',
+      author_email='ajoshi@sipi.usc.edu',
+      license='MIT',
+      packages=['brainsync'],
+      zip_safe=False)
